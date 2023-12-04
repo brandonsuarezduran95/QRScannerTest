4209f741eb5177457446d4af9db5ebec7b758105
Brandon Suarez <brandonsuarezduran@gmail.com>
2023-12-03 22:49:53 -0500

    Initial Commit

---
+++ QRScannerTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QRScannerTest
+//
+//  Created by Brandon Suarez on 12/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QRScannerTest/QRScannerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScannerTestApp.swift
+//  QRScannerTest
+//
+//  Created by Brandon Suarez on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct QRScannerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
